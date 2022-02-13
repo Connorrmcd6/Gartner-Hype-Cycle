5b9ef8566bc1da5c5cb7e59da7acec66c7795020
Connor McDonald <connormcd98@gmail.com>
2022-02-13 08:56:03 +0200

    adding time of failure

--- Data/alert.py
+++ Data/alert.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 from functions import *
 
 cnxn = mysql.connector.connect(host = ip, user = user, password = passwd, database = schema1)
@@ -13,11 +14,19 @@ cursor.execute(statement2)
 old_count = cursor.fetchone()[0]
 
 if new_count == old_count:
-    message = '🚨 *TWITTER DATA COLLECTION HAS STOPPED!*'
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y %H:%M")
+    dt_string = str(dt_string)
+    dt_string = dt_string.replace('-', '\\-')
+    message = f'🚨 *TWITTER DATA COLLECTION HAS STOPPED\\!*\nLast Checked: {dt_string}'
     telegram_bot_sendtext(message, chat_id)
+    statement3 = 'INSERT INTO social.logs (tweet_count) VALUES (' + str(new_count) + ')'
+    cursor.execute(statement3)
+    cnxn.commit()
     print('stopped')
 
 else:
+    x = ''
     statement3 = 'INSERT INTO social.logs (tweet_count) VALUES (' + str(new_count) + ')'
     cursor.execute(statement3)
     cnxn.commit()
