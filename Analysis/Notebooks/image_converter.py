028a2a5f634ebe878559e4986caf0b755820edba
Connor McDonald <connormcd98@gmail.com>
2022-03-06 13:05:44 +0200

    pngs

--- Analysis/Notebooks/image_converter.py
+++ Analysis/Notebooks/image_converter.py
@@ -1,6 +1,24 @@
 from pdf2image import convert_from_path
-images = convert_from_path('/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/zipf_plot.pdf', 300)
+
+i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/zipf_plot.pdf'
+i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neg_vs_positive.pdf'
+i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neghmean_vs_poshmean.pdf'
+i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/normcdf_hmean.pdf'
+
+images = convert_from_path(i1, 300)
 for image in images:
     image.save('zipfs_plot.png')
+
+# images = convert_from_path(i2, 300)
+# for image in images:
+#     image.save('neg_vs_positive.png')
+
+# images = convert_from_path(i3, 300)
+# for image in images:
+#     image.save('neghmean_vs_poshmean.png')
+
+# images = convert_from_path(i4, 300)
+# for image in images:
+#     image.save('normcdf_hmean.png')
 
 print('done')
