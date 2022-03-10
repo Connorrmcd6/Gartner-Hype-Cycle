29e9221840265ba2d9cc0054cc2491062d1c4c99
Connor McDonald <connormcd98@gmail.com>
2022-03-10 06:42:12 +0200

    presentation

--- Analysis/Notebooks/image_converter.py
+++ Analysis/Notebooks/image_converter.py
@@ -1,24 +1,24 @@
 from pdf2image import convert_from_path
 
-i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/zipf_plot.pdf'
-i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neg_vs_positive.pdf'
-i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neghmean_vs_poshmean.pdf'
-i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/normcdf_hmean.pdf'
+i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/google_covid.pdf'
+i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/3dprint.pdf'
+i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/google_blockchain.pdf'
+i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/google_ml.pdf'
 
 images = convert_from_path(i1, 300)
 for image in images:
-    image.save('zipfs_plot.png')
+    image.save('covid.png')
 
 # images = convert_from_path(i2, 300)
 # for image in images:
-#     image.save('neg_vs_positive.png')
+#     image.save('3dprint.png')
 
 # images = convert_from_path(i3, 300)
 # for image in images:
-#     image.save('neghmean_vs_poshmean.png')
+#     image.save('google_blockchain.png')
 
 # images = convert_from_path(i4, 300)
 # for image in images:
-#     image.save('normcdf_hmean.png')
+#     image.save('google_ml.png')
 
 print('done')
