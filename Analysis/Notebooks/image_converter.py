573572090925fe1e82d818068862ab130edf0f1e
Connor McDonald <connormcd98@gmail.com>
2022-03-13 11:02:32 +0200

    vis

--- Analysis/Notebooks/image_converter.py
+++ Analysis/Notebooks/image_converter.py
@@ -1,24 +1,24 @@
 from pdf2image import convert_from_path
 
-i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/zipf_plot.pdf'
+i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neg_sentiment_count.pdf'
 i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neg_vs_positive.pdf'
 i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neghmean_vs_poshmean.pdf'
 i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/normcdf_hmean.pdf'
 
 images = convert_from_path(i1, 300)
 for image in images:
-    image.save('zipf_plot.png')
+    image.save('neg_sentiment_count.png')
 
-images = convert_from_path(i2, 300)
-for image in images:
-    image.save('neg_vs_positive.png')
+# images = convert_from_path(i2, 300)
+# for image in images:
+#     image.save('neg_vs_positive.png')
 
-images = convert_from_path(i3, 300)
-for image in images:
-    image.save('neghmean_vs_poshmean.png')
+# images = convert_from_path(i3, 300)
+# for image in images:
+#     image.save('neghmean_vs_poshmean.png')
 
-images = convert_from_path(i4, 300)
-for image in images:
-    image.save('normcdf_hmean.png')
+# images = convert_from_path(i4, 300)
+# for image in images:
+#     image.save('normcdf_hmean.png')
 
 print('done')
