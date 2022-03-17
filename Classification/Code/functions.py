132f9b88d60d3a356a71051679e673a090129577
Connor McDonald <connormcd98@gmail.com>
2022-03-17 06:42:10 +0200

    nb

--- Classification/Code/functions.py
+++ Classification/Code/functions.py
@@ -3,6 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from textblob import TextBlob
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+# ML Libraries
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 
 def base_subjectivity(text):
@@ -34,3 +47,46 @@ def sentibar(df):
     df['sentiment'].value_counts().plot(kind='bar')
     plt.show()
 
+
+# Global Parameters
+stop_words = set(stopwords.words('english'))
+
+def load_dataset(filename, cols):
+    dataset = pd.read_csv(filename, encoding='latin-1')
+    dataset.columns = cols
+    return dataset
+
+def remove_unwanted_cols(dataset, cols):
+    for col in cols:
+        del dataset[col]
+    return dataset
+
+def preprocess_tweet_text(tweet):
+    tweet.lower()
+    # Remove urls
+    tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+    # Remove user @ references and '#' from tweet
+    tweet = re.sub(r'\@\w+|\#','', tweet)
+    # Remove punctuations
+    tweet = tweet.translate(str.maketrans('', '', string.punctuation))
+    # Remove stopwords
+    tweet_tokens = word_tokenize(tweet)
+    filtered_words = [w for w in tweet_tokens if not w in stop_words]
+    
+    ps = PorterStemmer()
+    stemmed_words = [ps.stem(w) for w in filtered_words]
+    lemmatizer = WordNetLemmatizer()
+    lemma_words = [lemmatizer.lemmatize(w, pos='a') for w in stemmed_words]
+    
+    return " ".join(filtered_words)
+
+def get_feature_vector(train_fit):
+    vector = TfidfVectorizer(sublinear_tf=True)
+    vector.fit(train_fit)
+    return vector
+
+def convert_sent(sentiment):
+    if sentiment == 2:
+        return 1
+    elif sentiment == 4:
+        return 2
