5f0f76420b7ded8192de94533104e3b744f66281
Connor McDonald <connormcd98@gmail.com>
2022-06-18 08:18:16 +0200

    a

--- Analysis/Notebooks/image_converter.py
+++ Analysis/Notebooks/image_converter.py
@@ -1,17 +1,17 @@
 from pdf2image import convert_from_path
 
-i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/SVM3.pdf'
-i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neg_vs_positive.pdf'
-i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neghmean_vs_poshmean.pdf'
-i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/normcdf_hmean.pdf'
+i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/SVM1.pdf'
+i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/SVM2.pdf'
+# i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neghmean_vs_poshmean.pdf'
+# i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/normcdf_hmean.pdf'
 
 images = convert_from_path(i1, 300)
 for image in images:
-    image.save('SVM3.png')
+    image.save('SVM1.png')
 
-# images = convert_from_path(i2, 300)
-# for image in images:
-#     image.save('neg_vs_positive.png')
+images = convert_from_path(i2, 300)
+for image in images:
+    image.save('SVM2.png')
 
 # images = convert_from_path(i3, 300)
 # for image in images:
