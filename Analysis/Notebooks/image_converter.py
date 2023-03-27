3f0e7fc7a272828074bb9734c7f2ef4ce8d778ca
Connor McDonald <connormcd98@gmail.com>
2023-03-27 09:02:28 +0200

    journal

--- Analysis/Notebooks/image_converter.py
+++ Analysis/Notebooks/image_converter.py
@@ -1,17 +1,17 @@
 from pdf2image import convert_from_path
 
-i1 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/bc_validation.pdf'
-i2 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/ml_validation.pdf'
+i1 = '/Users/connormcdonald/Downloads/80-20.pdf'
+i2 = '/Users/connormcdonald/Downloads/zipf_sample.pdf'
 # i3 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/neghmean_vs_poshmean.pdf'
 # i4 = '/Users/connormcdonald/Desktop/Masters/MIT807/Gartner Repository/Analysis/Figures/normcdf_hmean.pdf'
 
 images = convert_from_path(i1, 300)
 for image in images:
-    image.save('/Users/connormcdonald/Desktop/Masters/MIT807/Final/bc_validation.png')
+    image.save('/Users/connormcdonald/Downloads/80-20.png')
 
 images = convert_from_path(i2, 300)
 for image in images:
-    image.save('/Users/connormcdonald/Desktop/Masters/MIT807/Final/ml_validation.png')
+    image.save('/Users/connormcdonald/Downloads/zipf_sample.png')
 
 # images = convert_from_path(i3, 300)
 # for image in images:
